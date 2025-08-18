801966c451823f9c16d9c4355401fe8610596728
Weyne <augustoweyne12@gmail.com>
2025-08-18 07:07:53 -0300

    feature: iniciado JS

--- src/module/form/show-form.js
+++ src/module/form/show-form.js
@@ -1,13 +1,32 @@
 const btNewSchedule = document.getElementById("bt-new-schedule");
 const sectionForm = document.getElementById("new-schedule");
 const sectionMain = document.getElementById("app");
+const body = document.querySelector("body");
 
 btNewSchedule.addEventListener("click", (event) => {
+    event.target.closest("div").classList.toggle("display-none");
+    alterShowForm();
+});
+
+body.addEventListener("click", (event) => {
+    console.log(sectionForm.childNodes)
+    sectionForm.childNodes.forEach((node) => {
+        console.log(node, node != event.target);
+    })
+
+    if(event.target != btNewSchedule &&
+      sectionForm.classList.contains("display-none")){
+        console.log("entro")
+        alterShowForm();
+        btNewSchedule.closest("div").classList.toggle("display-none");
+    }
+});
+
+function alterShowForm() {
     sectionMain.classList.toggle("blur");
     sectionForm.classList.toggle("display-none");
-
+    
     setTimeout(() => {
         sectionForm.classList.toggle("opacity-0");
-        btNewSchedule.classList.toggle("opacity-0");
-    }, 1);
-})
+    }, 10);
+}
